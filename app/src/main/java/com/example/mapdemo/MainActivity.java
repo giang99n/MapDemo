f003b319f52ccf5107719f0dd7e787f35bd7ea1e
giang99n <62471607+giang99n@users.noreply.github.com>
2020-08-09 22:13:52 +0700

    mapDemo 1

---
+++ app/src/main/java/com/example/mapdemo/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.mapdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.mapdemo.directionhelpers.FetchURL;
+import com.example.mapdemo.directionhelpers.TaskLoadedCallback;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, TaskLoadedCallback {
+
+    GoogleMap map;
+    Button btn;
+    MarkerOptions place1,place2;
+    private Polyline currentPolyline;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn=findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new FetchURL(MainActivity.this).execute(getUrl(place1.getPosition(), place2.getPosition(), "driving"), "driving");
+            }
+        });
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        place1 = new MarkerOptions().position(new LatLng(21.0324008,105.7807066)).title("Location1");
+        place2 = new MarkerOptions().position(new LatLng(21.004675, 105.841479)).title("Location2");
+
+
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        map.addMarker(place1);
+        map.addMarker(place2);
+        map.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(21.0324008,105.7807066)));
+    }
+    private String getUrl(LatLng origin, LatLng dest, String directionMode) {
+        // Origin of route
+        String str_origin = "origin=" + origin.latitude + "," + origin.longitude;
+        // Destination of route
+        String str_dest = "destination=" + dest.latitude + "," + dest.longitude;
+        // Mode
+        String mode = "mode=" + directionMode;
+        // Building the parameters to the web service
+        String parameters = str_origin + "&" + str_dest + "&" + mode;
+        // Output format
+        String output = "json";
+        // Building the url to the web service
+        String url = "https://maps.googleapis.com/maps/api/directions/" + output + "?" + parameters + "&key=" + getString(R.string.key_map);
+        return url;
+    }
+
+    @Override
+    public void onTaskDone(Object... values) {
+        if (currentPolyline != null)
+            currentPolyline.remove();
+        currentPolyline = map.addPolyline((PolylineOptions) values[0]);
+    }
+}
---
+++ app/src/main/java/com/example/mapdemo/directionhelpers/TaskLoadedCallback.java
@@ -0,0 +1,5 @@
+package com.example.mapdemo.directionhelpers;
+
+public interface TaskLoadedCallback {
+    void onTaskDone(Object... values);
+}
